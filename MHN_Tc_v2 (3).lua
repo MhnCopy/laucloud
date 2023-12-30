0abd17aa534cd982b0d32738f7259c629b20ba73
MhnCopy <154570997+MhnCopy@users.noreply.github.com>
2023-12-31 02:01:42 +0800

    Add files via upload

---
+++ MHN_Tc_v2 (3).lua
@@ -0,0 +1,493 @@
+function Main()
+menu1 = gg.choice({
+"1.遊戲加速",
+"2.解除全圖",
+"3.解除移動速限",
+"4.部位破壞",
+"5.全部(1+2+3+4)",
+"6.原版解除移動速度",
+"退出"
+}, nil, "By 炭球")
+if menu1 == 1 then byx1() end
+if menu1 == 2 then byx2() end
+if menu1 == 3 then byx3() end
+if menu1 == 4 then byx4() end
+if menu1 == 5 then byx5() end
+if menu1 == 6 then byx6() end
+if menu1 == 8  then Exit() end
+XGCK = -1
+end
+
+function byx1()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1;0.33333334327;0.02999999933::12", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("3", gg.TYPE_FLOAT)
+gg.toast("遊戲加速:OK")
+gg.clearResults()
+end
+
+function byx2()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1.40129846e-45;1;0;0;0;0;0;0;2.80259693e-45::44", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("8", gg.TYPE_FLOAT)
+gg.toast("解除全圖:OK")
+gg.clearResults()
+end
+
+function byx3()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1;1120403456;0;2000::20", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0", gg.TYPE_DWORD)
+gg.toast("解除移動速限:OK")
+gg.clearResults()
+end
+
+function byx4()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1.40129846e-43;0.15000000596::8", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.40129846e-43", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("1.40129846e-39", gg.TYPE_FLOAT)
+gg.searchNumber("1.40129846e-39;0.15000000596::8", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.15000000596", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("99999999", gg.TYPE_FLOAT)
+gg.toast("部位破壞:OK")
+gg.clearResults()
+end
+
+function byx5()
+  byx1()
+  byx2()
+  byx3()
+  byx4()
+end
+
+
+
+function byx6()
+valueFromClass("SpeedLimitSettingsProto", "0x10", false, false, gg.TYPE_DWORD)
+  gg.getResults(9999)
+  gg.editAll(0, gg.TYPE_DWORD)
+  gg.clearResults()
+  stopClose()
+end
+
+function byx999()
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("特征码", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("改善", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("修改的数值", gg.TYPE_DWORD)
+gg.toast("完成后的提示")
+gg.clearResults()
+end
+
+
+--原始
+
+function valueFromClass(class, offset, tryHard, bit32, valueType)
+Get_user_input = {}
+Get_user_input[1] = class
+Get_user_input[2] = offset
+Get_user_input[3] = tryHard
+Get_user_input[4] = bit32
+Get_user_type = valueType
+start()
+end
+
+function start()
+user_input_taker()
+O_initial_search()
+O_dinitial_search()
+if error > 1 then
+return 0
+end
+CA_pointer_search()
+if error > 1 then
+return 0
+end
+CA_apply_offset()
+if error > 1 then
+return 0
+end
+Q_apply_fix()
+if error > 1 then
+return 0
+end
+if offst == 0 then
+A_base_accuracy()
+end
+if error > 1 then
+return 0
+end
+A_user_given_offset()
+if error > 1 then
+return 0
+end
+loopCheck()
+if error > 1 then
+return 0
+end
+end
+
+
+
+
+function loopCheck()
+if userMode == 1 then
+UI()
+elseif error == 3 then
+stopClose()
+end
+end
+
+
+function stopClose()
+while true do
+Main()
+gg.setVisible(false)
+while gg.isVisible() == false do
+end
+end
+end
+
+
+function A_user_given_offset()
+local old_save_list = gg.getListItems()
+for i, v in ipairs(old_save_list) do
+v.address = v.address + Get_user_input[2]
+v.flags = Get_user_type
+end
+gg.clearResults()
+gg.clearList()
+gg.loadResults(old_save_list)
+count = gg.getResultsCount()
+if count == 0 then
+found_("Q_apply_fix++")
+return 0
+end
+gg.setVisible(true)
+end
+
+
+
+function A_base_accuracy()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.loadResults(gg.getListItems())
+gg.clearList()
+gg.searchPointer(offst)
+count = gg.getResultsCount()
+if count == 0 then
+found_("A_base_accuracy")
+return 0
+end
+kol = gg.getResults(count)
+i = 1
+h = {}
+while (i-1) < count do
+h[i] = {}
+h[i].address = kol[i].value
+h[i].flags = 32
+i = i + 1
+end
+gg.addListItems(h)
+end
+
+
+function Q_apply_fix()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.loadResults(gg.getListItems())
+gg.clearList()
+count = gg.getResultsCount()
+if count == 0 then
+found_("Q_apply_fix")
+return 0
+end
+yy = gg.getResults(1000)
+gg.clearResults()
+i = 1
+c = 1
+s = {}
+while (i-1) < count do
+yy[i].address = yy[i].address + 0xb400000000000000
+gg.searchNumber(yy[i].address, gg.TYPE_QWORD)
+cnt = gg.getResultsCount()
+if 0 < cnt then
+bytr = gg.getResults(cnt)
+n = 1
+while (n-1) < cnt do
+s[c] = {}
+s[c].address = bytr[n].address
+s[c].flags = 32
+n = n + 1
+c = c + 1
+end
+end
+gg.clearResults()
+i = i + 1
+end
+gg.addListItems(s)
+end
+
+
+function CA_apply_offset()
+if Get_user_input[4] then
+tanker = 0xfffffffffffffff8
+else
+tanker = 0xfffffffffffffff0
+end
+local copy = false
+local l = gg.getListItems()
+if not copy then gg.removeListItems(l) end
+for i, v in ipairs(l) do
+	v.address = v.address + tanker
+	if copy then v.name = v.name..' #2' end
+end
+gg.addListItems(l)
+end
+
+
+function O_dinitial_search()
+if error > 1 then
+gg.setRanges(gg.REGION_C_ALLOC)
+else
+gg.setRanges(gg.REGION_OTHER)
+end
+gg.searchNumber(user_input, gg.TYPE_BYTE)
+count = gg.getResultsCount()
+if count == 0 then
+found_("O_dinitial_search")
+return 0
+end
+Refiner = gg.getResults(1)
+gg.refineNumber(Refiner[1].value, gg.TYPE_BYTE)
+count = gg.getResultsCount()
+if count == 0 then
+found_("O_dinitial_search")
+return 0
+end
+val = gg.getResults(count)
+gg.addListItems(val)
+end
+
+function user_input_taker()
+::stort::
+gg.clearResults()
+if userMode == 1 then
+if Get_user_input == nil then
+default1 = "PlayerController"
+default2 = "0x148"
+default3 = false
+default4 = false
+else
+default1 =Get_user_input[1]
+default2 = Get_user_input[2]
+default3 = Get_user_input[3]
+default4 = Get_user_input[4]
+end
+Get_user_input = gg.prompt(
+{"🔰Script By🔰: https://t.me/Hackers_House_YT\n\nClass Name: ", "Offset: ","Try Harder --(decreases accuracy)","Try For 32 bit"},
+{default1,default2,default3,default4},
+{"text","text","checkbox","checkbox"})
+if Get_user_input ~= nil then
+if (Get_user_input[1] == "") or (Get_user_input[2] == "") then
+gg.alert("ℹ️ Don't Leave Input Blankℹ️")
+goto stort
+end
+else
+gg.alert("ℹ️ Error : Try again ℹ️")
+goto stort
+end
+Get_user_type = gg.choice({"1. Byte / Boolean","2. Dword / 32 bit Int","3. Qword / 64 bit Int","4. Float","5. Double"},nil,"🔰Script By🔰: https://t.me/Hackers_House_YT\n\nℹ️ Choose The Output Type ℹ️")
+if Get_user_type == 1 then
+Get_user_type = gg.TYPE_BYTE
+elseif Get_user_type == 2 then
+Get_user_type = gg.TYPE_DWORD
+elseif Get_user_type == 3 then
+Get_user_type = gg.TYPE_QWORD
+elseif Get_user_type == 4 then
+Get_user_type = gg.TYPE_FLOAT
+elseif Get_user_type == 5 then
+Get_user_type = gg.TYPE_DOUBLE
+end
+if Get_user_type ~= gg.TYPE_BYTE then
+if (Get_user_input[2] % 4) ~= 0 then
+gg.alert("ℹ️Hex Offset Must Be An Multiple OF 4ℹ️")
+goto stort
+end
+end
+end
+error = 0 
+end
+
+function O_initial_search()
+gg.setVisible(false)
+gg.toast("Wait a Moment")
+user_input = ":"..Get_user_input[1] 
+if Get_user_input[3] then
+offst = 25
+else
+offst = 0
+end
+end
+
+function CA_pointer_search()
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC | gg.REGION_OTHER)
+gg.loadResults(gg.getListItems())
+gg.searchPointer(offst)
+count = gg.getResultsCount()
+if count == 0 then
+found_("CA_pointer_search")
+return 0
+end
+vel = gg.getResults(count)
+gg.clearList()
+gg.addListItems(vel)
+end
+
+function found_(message)
+if error == 1 then
+found2(message)
+elseif error == 2 then
+found3(message)
+elseif error == 3 then
+found4(message)
+else
+found(message)
+end
+end
+
+
+
+function found(message)
+if count == 0 then
+gg.clearResults()
+gg.clearList()
+first_error = message
+error = 1
+second_start()
+end
+end
+
+function second_start()
+user_input_taker()
+O_initial_search()
+O_dinitial_search()
+if error > 0 then
+return 0
+end
+CA_pointer_search()
+if error > 0 then
+return 0
+end
+CA_apply_offset()
+if error > 0 then
+return 0
+end
+A_base_value()
+if error > 0 then
+return 0
+end
+if offst == 0 then
+A_base_accuracy()
+end
+if error > 0 then
+return 0
+end
+A_user_given_offset()
+if error > 0 then
+return 0
+end
+loopCheck()
+if error > 0 then
+return 0
+end
+end
+
+
+function A_base_value()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.loadResults(gg.getListItems())
+gg.clearList()
+gg.searchPointer(offst)
+count = gg.getResultsCount()
+if count == 0 then
+found_("A_base_value")
+return 0
+end
+tel = gg.getResults(count)
+gg.addListItems(tel)
+end
+
+
+
+
+function found2(message)
+if count == 0 then
+gg.clearResults()
+gg.clearList()
+second_error = message
+error = 2
+third_start()
+end
+end
+
+
+
+
+
+function found3(message)
+if count == 0 then
+gg.clearResults()
+gg.clearList()
+third_error = message
+error = 3
+fourth_start()
+end
+end
+
+
+
+
+
+function found4(message)
+if count == 0 then
+gg.clearResults()
+gg.clearList()
+gg.alert("❌Value NOT FOUND❌")
+gg.setVisible(true)
+loopCheck()
+end
+end
+
+
+
+--原始 end
+
+function Exit()
+print("脚本退出了")
+os.exit()
+end
+while true do
+if gg.isVisible(true) then
+XGCK = 1
+gg.setVisible(false)
+end
+gg.clearResults()
+if XGCK == 1 then
+Main()
+end
+end
